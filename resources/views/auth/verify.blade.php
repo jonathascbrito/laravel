4ba81231b30406419179d8ab23210b06db5cd86c
Jonathas Brito <jonathas.brito@outlook.com>
2022-07-27 18:55:08 -0300

    Configurado o modulo de verificação de email para cadastro, com criação da classe notification relacionada

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Falta pouco agora! Precisamos apenas que você valide o seu e-mail.') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Reenviamos um link para você com um link de validação.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Antes de utilizar os recursos da aplicação, por favor valide seu e-mail por meio do link de verificação que encaminhamos para o seu email.') }}
+                    {{ __('Caso voc~e não tenha recebido o e-mail de verificação por favor clique no link abaixo.') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('clique aqui para solicitar outro e-mail de verificação.') }}</button>.
                     </form>
                 </div>
             </div>
